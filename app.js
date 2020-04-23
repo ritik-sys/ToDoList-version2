b677713951142de7128a14dc860873bd234f1e0c
ritik-sys <60854557+ritik-sys@users.noreply.github.com>
2020-04-23 19:52:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,185 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+//initializing method to our modules
+const app = express();
+mongoose.connect("mongodb+srv://admin-ritik:test123@cluster0-t4jbz.mongodb.net/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+
+//making a schema in todolistDB
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+
+//adding a collection of itemSchema
+const Item = mongoose.model("Item", itemsSchema);
+
+
+
+//adding items ot Item collection
+const item1 = new Item({
+  name: "Buy Food"
+});
+const item2 = new Item({
+  name: "Made Food"
+});
+const item3 = new Item({
+  name: "Eat Food"
+});
+
+//pushing items in an array
+const defaultItems = [item1, item2, item3];
+
+//making another schema for storing lists
+const listSchema = new mongoose.Schema({
+  name: String,
+  items: [itemsSchema]
+});
+//adding a collection called list which is of itemSchema type
+const List = mongoose.model("List", listSchema);
+
+
+
+
+app.get("/", function(req, res) {
+
+
+  Item.find({}, function(err, foundItems) {
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("sucessfully added");
+        }
+      });
+      res.redirect("/")
+    } else {
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: foundItems
+      });
+    }
+  });
+
+});
+
+
+
+
+
+
+app.post("/", function(req, res) {
+
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+  //make a new item to be added
+  const item = new Item({
+    name: itemName
+  });
+  if (listName === "Today") {
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    })
+  }
+
+
+
+
+
+});
+
+
+app.post("/delete", function(req, res) {
+      const checkedItemId = req.body.checkbox;
+      const listName = req.body.listName;
+      if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemId, function(err) {
+          if (!err) {
+            console.log("sucess");
+            res.redirect("/");
+          }
+        });
+      } else {
+        List.findOneAndUpdate({
+            name: listName
+          }, {
+            $pull: {
+              items: {
+                _id: checkedItemId
+              }
+            }
+          },
+            function(err, foundList) {
+              if (!err) {
+                res.redirect("/" + listName+"");
+              }
+
+          });
+        };
+
+});
+
+
+
+      app.get("/:customListName", function(req, res) {
+        const customListName = _.capitalize(req.params.customListName);
+        List.findOne({
+          name: customListName
+        }, function(err, foundList) {
+          if (!err) {
+            if (!foundList) {
+              //create a new list
+
+              const list = new List({
+                name: customListName,
+                items: defaultItems
+              });
+              list.save();
+              res.redirect("/" + customListName + "")
+            } else {
+              //show the existing list
+              res.render("list", {
+                listTitle: foundList.name,
+                newListItems: foundList.items
+              });
+
+            }
+          }
+        });
+      });
+
+
+
+
+
+
+
+
+
+      app.get("/about", function(req, res) {
+        res.render("about");
+      });
+
+      app.listen(process.env.PORT||3000, function() {
+        console.log("Server started on port 3000");
+      });
